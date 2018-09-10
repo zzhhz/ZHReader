4d387a01d77f151412f1fe93a3815249b56302c0
墨宇 <zzh_hz@126.com>
2018-09-10 21:48:56 +0800

    更新gradle 插件

--- app/src/main/java/com/zzh/reader/dao/DaoMaster.java
+++ app/src/main/java/com/zzh/reader/dao/DaoMaster.java
@@ -21,16 +21,16 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
-        BookDao.createTable(db, ifNotExists);
         BookMarkDao.createTable(db, ifNotExists);
         CatalogueDao.createTable(db, ifNotExists);
+        BookDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
-        BookDao.dropTable(db, ifExists);
         BookMarkDao.dropTable(db, ifExists);
         CatalogueDao.dropTable(db, ifExists);
+        BookDao.dropTable(db, ifExists);
     }
 
     /**
@@ -49,9 +49,9 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
-        registerDaoClass(BookDao.class);
         registerDaoClass(BookMarkDao.class);
         registerDaoClass(CatalogueDao.class);
+        registerDaoClass(BookDao.class);
     }
 
     public DaoSession newSession() {
